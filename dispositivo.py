83d3136ce0e0d6214313d698a0b87326ee3fa542
Marcos <marcos@omegasistemas.com.br>
2022-09-16 22:25:04 -0300

    Adicionado Método Subscribed

--- dispositivo.py
+++ dispositivo.py
@@ -3,10 +3,19 @@ import time
 from hal import temperatura, umidade, aquecedor
 from definitions import usuario, password, client_id, server, port
 
+
+def mensagem(client, user, msg):
+    if msg.topic == 'pucpr/iotmc/alarcon/aquecedor':
+        aquecedor(msg.payload.decode())
+
 # Conexão inicial
 client = mqtt.Client(client_id)
 client.username_pw_set(usuario, password)  
 client.connect(server, port)
+
+client.on_message = mensagem
+client.subscribe('pucpr/iotmc/alarcon/aquecedor')
+client.loop_start()
 
 # Comportamento do sistema
 while True:
