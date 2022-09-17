806b7348e6e8d94bdd6b07ed11b8d7a5fcefb909
Marcos <marcos@omegasistemas.com.br>
2022-09-16 21:32:22 -0300

    Primeiro Commit

---
+++ dispositivo.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+import time
+
+    
+usuario = ''
+password = ''
+client_id = 'xuxumelancia'
+server = 'broker.mqttdashboard.com'
+port = 1883
+
+client = mqtt.Client(client_id)
+client.username_pw_set(usuario, password)  
+client.connect(server, port)
+
+client.publish('pucpr/iotmc/alarcon/temperatura', '24.1')
+time.sleep(1)
+  
+
+client.disconnect()
