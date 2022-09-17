0afccc6d0ac84e26c832f6e487097bdec4568b9b
Marcos <marcos@omegasistemas.com.br>
2022-09-16 22:00:05 -0300

    Criado função Umidade e arquivo de funções

--- dispositivo.py
+++ dispositivo.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
-
+from hal import temperatura, umidade, aquecedor
 
 usuario = ''
 password = ''
@@ -8,13 +8,13 @@ client_id = 'xuxumelancia'
 server = 'broker.mqttdashboard.com'
 port = 1883
 
-
 client = mqtt.Client(client_id)
 client.username_pw_set(usuario, password)  
 client.connect(server, port)
 
-client.publish('pucpr/iotmc/alarcon/temperatura', '24.1')
-time.sleep(1)
+while True:
+    client.publish('pucpr/iotmc/alarcon/temperatura', temperatura())
+    client.publish('pucpr/iotmc/alarcon/umidade', umidade())
+    time.sleep(1)
   
-
-client.disconnect()
+# client.disconnect()
